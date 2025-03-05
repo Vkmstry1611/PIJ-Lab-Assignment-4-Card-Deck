0521b661fce3302aeb972b84fe2f95b81f73a88f
unknown <vidish162005@gmail.com>
2025-03-05 14:44:31 +0530

    initialize Deck.java

---
+++ Deck.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Deck {
+    private ArrayList<Card> deck;
+
+    public Deck() {
+        deck = new ArrayList<>();
+        createDeck(); // Creating deck on object initialization
+    }
+
+}
