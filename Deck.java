b13c06ee1356f4ef26e657bcd176ed8193ee5e5f
unknown <vidish162005@gmail.com>
2025-03-05 14:47:00 +0530

    created create deck function

--- Deck.java
+++ Deck.java
@@ -5,7 +5,19 @@ class Deck {
 
     public Deck() {
         deck = new ArrayList<>();
-        createDeck(); // Creating deck on object initialization
+
     }
 
+
+    // Method to create a deck of 52 cards
+    public void createDeck() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
 }
